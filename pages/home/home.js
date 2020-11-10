b8e33a627bce7fa0ee583c6cd80985dee05e2424
webuser145 <55380530+webuser145@users.noreply.github.com>
2020-11-10 14:43:31 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,32 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // 存放数据
+  data:{
+    name:'coderhe',
+    content:'cat',
+    movies:[
+      {id:'01',name:'are you ok',time:'3:33'},
+      {id:'02',name:'how are you',time:'1:23'},
+      {id:'03',name:'hello small',time:'1:90'},
+      {id:'04',name:'are you ok',time:'3:33'},
+      {id:'05',name:'are you ok',time:'3:33'},
+    ],
+    count:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 执行函数
+  addCount(){
+    // 1. 数据会发生变化，但是视图内容不会响应改变 pass
+    // this.data.count++;
+    // console.log(this.data.count);
+    // 2.  this.setData() success
+    this.setData({
+      // count:10
+      count:this.data.count += 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  subCount(){
+    this.setData({
+      count:this.data.count -= 1
+    })
   }
 })
